d8a96097dc3379d43a0accfee95409606869ebd3
ragvri <raghavjindal2003@gmail.com>
2019-11-06 15:59:41 -0500

    todo 1) Menu items add and updat
    2) home button in each page
    
    3) critic/review can't like his review?

--- webserver/server.py
+++ webserver/server.py
@@ -102,35 +102,27 @@ def restaurants():
     request.form:     if the browser submitted a form, this contains the data in the form
     request.args:     dictionary of URL arguments, e.g., {a:1, b:2} for http://localhost?a=1&b=2
     """
+    logged_in = False
     if session['logged_in']:
-        print(session['userid'])
+        logged_in = True
 
     if request.method == 'POST' and request.form["button"] == 'Search':
-        print(request.form['button'])
-        print(f'The method is post')
         search_name = request.form['name']  # name of the search query
-        print(f'search name is : {search_name}')
         cursor = g.conn.execute(f"""
-        SELECT * FROM Restaurant_owns r
-        WHERE LOWER(r.name) LIKE '%%{search_name}%%'"""
+        SELECT * FROM test_restaurant_owns r
+        WHERE LOWER(r.name) LIKE '%%{search_name}%%'
+        ORDER BY lower(r.name) ASC 
+        """
                                 )
         names = []
         for result in cursor:
-            names.append(result)  # can also be accessed using result[0]
+            names.append(result)
         cursor.close()
 
-        # name = request.form['name']
-        # g.conn.execute('INSERT INTO test(name) VALUES (%s)', name)
-
-    #
-
-    #
-    # example of a database query
-    #
-    # cursor = g.conn.execute("SELECT name FROM test")
     else:
         cursor = g.conn.execute('''
-        SELECT * FROM Restaurant_owns r 
+        SELECT * FROM test_restaurant_owns r
+        ORDER BY lower(r.name) ASC 
         ''')
         names = []
         for result in cursor:
@@ -162,7 +154,7 @@ def restaurants():
     #     <div>{{n}}</div>
     #     {% endfor %}
     #
-    context = {'data': names}
+    context = {'data': names, 'logged_in': logged_in}
     #
     # render_template looks in the templates/ folder for files.
     # for example, the below file reads template/restaurants.html
@@ -178,30 +170,105 @@ def login():
     if request.method == "POST":
         username = request.form['username']
         password = request.form['password']
-        print("Details", username, password)
         cursor = g.conn.execute(
             "SELECT * FROM Users WHERE userid='{}' and password='{}'".format(username, password))
         counter = 0
         for _ in cursor:
             counter += 1
         if counter == 0:
-            print("curson None")
             error = True
             return render_template('login.html', error=error)
         else:
-            print("Logged In")
             session['logged_in'] = True
             session['userid'] = request.form['username']
             return redirect(url_for('restaurants'))
     return render_template('login.html', error=error)
 
 
-@app.route('/restaurants/<id>')
+@app.route('/restaurants/nearby/<lat>/<long>')
+def nearby(lat, long):
+    logged_in = False
+    if(session['logged_in']):
+        logged_in = True
+    cursor = g.conn.execute(f'''
+        SELECT r.rid, r.name, l.building, l.capacity, l.city, l.state, l.zip, calculate_distance({lat}, {long}, l.lat, l.long, 'K') as distance
+        FROM location_isat l, test_restaurant_owns r
+        WHERE l.lat!='{lat}' and l.long!='{long}' and r.rid=l.rid
+        ORDER BY distance
+        LIMIT 5
+    ''')
+
+    class restaurant():
+        def __init__(self, data):
+            self.rid = data[0]
+            self.name = data[1]
+            self.building = data[2]
+            self.capacity = data[3]
+            self.city = data[4]
+            self.state = data[5]
+            self.zip = data[6]
+            self.dis = round(data[7], 3)
+    n_restaurants = []
+    for results in cursor:
+        n_restaurants.append(restaurant(results))
+
+    context = {'nearby': n_restaurants, 'logged_in': logged_in}
+    return render_template('nearby.html', **context)
+
+
+@app.route('/restaurants/<id>', methods=['POST', 'GET'])
 def another(id):
+
+    # check if logged in. If yes, get the userid
+    logged_in = False
+    user_critic_logged = False
+    owner_logged = False
+    if(session['logged_in']):
+        logged_in = True
+        userid = session['userid']
+        if str(userid).startswith('n') or str(userid).startswith('c'):
+            user_critic_logged = True
+            print(f'USER critic logged in')
+        elif str(userid).startswith('r'):
+            owner_logged = True
+
+    # handle the case when a review is liked.
+    if request.method == 'POST':
+        # print(f"{request.form['normal_like']} this is the data")
+        if 'normal_like' in request.form:
+            revid = request.form['normal_like']
+            print(f'revid is {revid}')
+            cursor = g.conn.execute(f'''
+                UPDATE test_reviews_gives   
+                SET likes = likes+1
+                WHERE revid = '{revid}' 
+            ''')
+        elif 'critic_like' in request.form:
+            critic_id, revid = request.form['critic_like'].split('-')
+            print(f'critic_id: {critic_id}, revid:{revid}')
+            cursor = g.conn.execute(f'''
+                UPDATE test_reviews_gives 
+                SET likes = likes+1
+                WHERE revid = '{revid}' 
+            ''')
+            # update the average score of the critc
+            cursor = g.conn.execute(f'''
+                UPDATE test_critic
+                SET score = 
+                (
+                    SELECT round(avg(t.likes),2)
+                    FROM test_critic c, test_reviews_gives t
+                    where t.userid = c.userid and 
+                    c.userid = '{critic_id}'
+                    group by c.userid 
+                ) 
+                WHERE userid = '{critic_id}'
+            ''')
+            print(f'Updating the score of critic')
 
     # get the name and the rid of the url
     cursor = g.conn.execute(f'''
-        SELECT r.name, r.rid FROM Restaurant_owns r 
+        SELECT r.name, r.rid FROM test_restaurant_owns r 
         WHERE r.rid =  '{id}'
         ''')
     for result in cursor:
@@ -216,12 +283,11 @@ def another(id):
     ''')
     for location in cursor:
         locations.append(location)
-    print(locations)
     # get normal user reviews
     normal_reviews = []
     cursor = g.conn.execute(f'''
-        SELECT r.text,r.likes, r.rating, u.name, u.userid
-        FROM reviews_gives r, Users u
+        SELECT r.text,r.likes, r.rating, u.name, r.revid
+        FROM test_reviews_gives r, Users u
         WHERE r.userid IN 
         (SELECT n.userid FROM normal n)
         and r.userid = u.userid
@@ -230,15 +296,13 @@ def another(id):
     for review in cursor:
         normal_reviews.append(review)
 
-    print(normal_reviews)
-
     # get critic reviews
     critic_reviews = []
     cursor = g.conn.execute(f'''
-        SELECT r.text, r.likes, r.rating, u.name, u.userid
-        FROM reviews_gives r, Users u
+        SELECT r.text, r.likes, r.rating, u.name, u.userid, r.revid
+        FROM test_reviews_gives r, Users u
         WHERE r.userid IN 
-        (SELECT c.userid FROM critic c ) and 
+        (SELECT c.userid FROM test_critic c ) and 
         r.userid = u.userid and 
         r.rid ='{id}'
     ''')
@@ -261,12 +325,12 @@ def another(id):
     similar_restaurants = []
     cursor = g.conn.execute(f'''
         SELECT s1.rid2, r1.name 
-        from similar_rest s1, restaurant_owns r1 
+        from similar_rest s1, test_restaurant_owns r1 
         WHERE s1.rid1 = '{id}' 
         and r1.rid = s1.rid2
         UNION 
         SELECT s2.rid1, r2.name 
-        from similar_rest s2, restaurant_owns r2 
+        from similar_rest s2, test_restaurant_owns r2 
         WHERE s2.rid2 = '{id}'
         and r2.rid = s2.rid1
     ''')
@@ -274,25 +338,58 @@ def another(id):
     for restaurant in cursor:
         similar_restaurants.append(restaurant)
 
-    print(similar_restaurants)
-
     context = {'name': restaurant_name, 'id': restaurant_id, 'similar': similar_restaurants,
-               'menu': menu_items, 'critic_reviews': critic_reviews, 'normal_reviews': normal_reviews, 'locations': locations}
-    # context  ={'name':restaurant_name, 'id': restaurant_id}
-    print(restaurant_name)
+               'menu': menu_items, 'critic_reviews': critic_reviews,
+               'normal_reviews': normal_reviews, 'locations': locations,
+               'user_critic_logged_in': user_critic_logged, 'owner_logged': owner_logged,
+               'logged_in': logged_in}
     return render_template('restaurant_page.html', **context)
+
+
+@app.route('/give_review/<id>', methods=["GET", "POST"])
+def give_review(id):
+    if(session['logged_in']):
+        userid = session['userid']
+        if request.method == 'POST':
+            cursor = g.conn.execute(f'''
+                SELECT COUNT(*) 
+                FROM test_reviews_gives
+            ''')
+
+            for results in cursor:
+                revid = str(results[0]+1)
+
+            text = request.form['text']
+            rating = request.form['rating']
+            cursor = g.conn.execute(f'''
+                INSERT INTO test_reviews_gives
+                VALUES ('{revid}', '{text}', 0, {rating}, '{userid}', '{id}')
+            ''')
+
+            cursor = g.conn.execute(f'''
+                UPDATE test_restaurant_owns
+                SET u_rating = (SELECT ROUND(AVG(rating), 2)
+                                FROM test_reviews_gives
+                                WHERE rid='{id}' and userid like 'n%%' 
+                                GROUP BY rid),
+                c_rating = (SELECT ROUND(AVG(rating), 2)
+                                FROM test_reviews_gives
+                                WHERE rid='{id}' and userid like 'c%%' 
+                                GROUP BY rid)
+                WHERE rid = '{id}'
+            ''')
+            return redirect(f'/restaurants/{id}')
+        else:
+            return render_template('give_review.html')
+    return redirect(url_for('restaurants'))
 
 
 @app.route('/logout', methods=["GET", "POST"])
 def logout():
     if(session['logged_in']):
-        print("Method", request.method)
-        print("Session Details", session)
         if request.method == "POST":
-            print("Logging Out")
             session['logged_in'] = False
             session['userid'] = None
-            print("Session Details", session)
             return redirect(url_for('restaurants'))
         else:
             userid = session['userid']
@@ -310,9 +407,12 @@ def logout():
 
 @app.route('/critics/<id>')
 def critic(id):
+    logged_in = False
+    if session['logged_in']:
+        logged_in = True
     cursor = g.conn.execute(f'''
         SELECT u.name, c.score
-        FROM Critic as c, Users as u
+        FROM test_critic as c, Users as u
         WHERE c.userid = u.userid and c.userid = '{id}'
     ''')
     for result in cursor:
@@ -321,7 +421,7 @@ def critic(id):
 
     cursor = g.conn.execute(f'''
         SELECT r.rid, r.name, rv.text, rv.likes, rv.rating
-        FROM Critic as c, Reviews_gives as rv, Restaurant_owns as r
+        FROM test_critic as c, test_reviews_gives as rv, test_restaurant_owns as r
         WHERE c.userid = rv.userid and c.userid = '{id}' and rv.rid = r.rid
     ''')
 
@@ -348,7 +448,7 @@ def critic(id):
         critic_favrid = result[0]
         critic_fav = result[1]
 
-    context = {'name': critic_name, 'score': critic_score,
+    context = {'name': critic_name, 'score': critic_score, 'logged_in': logged_in,
                'favourite': critic_fav, 'fav_rid': critic_favrid, 'reviews': reviews}
     return render_template('critics.html', **context)
 
